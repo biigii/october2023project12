26d50aa77191f0c8b885d9ca0106fe026bb647e8
Behshad <82580731+biigii@users.noreply.github.com>
2023-11-23 19:12:19 +1300

    variables

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+
+// alert("hello world");
+// console.log('hello javascript');
+// console.log(2000);
+// console.log(1000+5000);
+// console.log(5*5)
+
+// var
+// const
+// let
+
+// declaring a variable
+// = symbol is for assignment and not for comparison
+// var color = 'red';
+
+// console.log(color);
+// console.log(color+color);
+
+// declare
+// var age;
+
+// age = 100;
+// console.log(age);
+
+// var age;
+// age = 10;
+// console.log(age);
+
+// const and let
+// you must initialize it and you can not assign new value 
+const age = 100;
+// age = 10;
+// console.log(age);
+
+//  let
+let color;
+// let color;
+color = '#f11'
+color = "blue"
+
+let backgroundColor;
+let fontSize;
+
+console.log(color);
+
+// types of variables
+
+// string
+const str = "hello world"
+const helloWorld = 'hello world'
+
+// numbers, integers or floats
+// snake casing my_awesome_variable
+// camel casing
+let myAwesomeVariable;
+
+let a = 100; // this is bad
+let myNum = 1000;
+
+// boolean which is true or false, 0 or 1, on or off
+const isDay = false
+const isValidCode = true;
+const value = false;
+
+// arrays or lists which is a series of things
+
+const list = [1, 2, 3, 4, 5];
+let fruits = ['apple', 'Orange', 'KIWI'];
+let myMixedArray = [100, 'hello', true, 0]
+
+console.log(fruits);
+
+// object
+const user = {
+  name: 'john',
+  age: 100,
+  isMarried: true,
+  address: {
+    no: 123,
+    street: 'street road',
+    city: 'christchurch'
+  }
+}
+
+console.log(user.address);
+console.log(user.name)
+console.log(user.address.city)
+
